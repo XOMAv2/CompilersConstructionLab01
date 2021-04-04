f7d863c812e267ffa2d2e9673d82d12113466dc6
Никита Иксарица <xomav2@gmail.com>
2021-04-04 18:24:01 +0300

    Добавлена функция визуализации графа в формате dot

--- src/cc/lab01/core.clj
+++ src/cc/lab01/core.clj
@@ -1,5 +1,18 @@
 (ns cc.lab01.core
+  (:require [cc.lab01.expressions :as ex]
+            [cc.lab01.morph :refer [nfa->dfa]]
+            [cc.lab01.utils :refer [graph->dot]])
   (:gen-class))
+
+(def graph (ex/succ (ex/alt (ex/sym "a")
+                            (ex/sym "b")
+                            (ex/sym "c")
+                            (ex/null))
+                    (ex/sym "c")))
+
+(spit "graph.dot" (graph->dot (nfa->dfa graph)))
+
+(spit "graph.dot" (graph->dot graph))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- src/cc/lab01/utils.clj
+++ src/cc/lab01/utils.clj
@@ -1,7 +1,41 @@
-(ns cc.lab01.utils)
+(ns cc.lab01.utils
+  (:require [clojure.set]))
 
 (defn final? [state]
   (if (set? state)
     (reduce #(or % @%2) false state)
     @state))
 
+(defn graph->dot [graph]
+  (let [kvs (map-indexed (fn [index state]
+                           [state (str index)])
+                         (:states graph))
+        names (apply conj {} kvs)
+        dotstring (str "digraph FSA {\n"
+                       "start [label=start shape=none height=0 width=0]\n"
+                       "start -> " (names (:start graph)) "\n")
+        finish-states (if (= (:graph-type graph) :DFA)
+                        (:finish graph)
+                        #{(:finish graph)})
+        dotstring (reduce (fn [text state]
+                            (str text (names state) " [shape=doublecircle]\n"))
+                          dotstring
+                          finish-states)
+        dotstring (reduce (fn [text state]
+                            (str text (names state) " [shape=circle]\n"))
+                          dotstring
+                          (clojure.set/difference (:states graph) finish-states))
+        dotstring (reduce-kv (fn [text start symboled-endings]
+                               (let [lines (for [[sym endings] symboled-endings
+                                                 :let [endings (if (= (:graph-type graph) :DFA)
+                                                                 #{endings}
+                                                                 endings)]
+                                                 finish endings]
+                                             (format "%s -> %s [label=%s]\n"
+                                                     (names start)
+                                                     (names finish)
+                                                     (if (= :null sym) "ε" sym)))]
+                                 (apply str text lines)))
+                             dotstring
+                             (:transitions graph))]
+    (str dotstring "}")))
